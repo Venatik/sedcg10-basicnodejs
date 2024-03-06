4d1500bd0633f3999082d55a5b4a88d3524e0871
Venatik <stefan.trajkovski008@gmail.com>
2024-03-06 10:31:34 +0100

    modified:   Class 04 - HTTP/notes-app/logs.txt 	modified:   Class 04 - HTTP/recap/script.js

--- Class 04 - HTTP/recap/script.js
+++ Class 04 - HTTP/recap/script.js
@@ -4,3 +4,12 @@ a2 = [4, 5, 6];
 
 console.log("a1:", a1); // [1, 2, 3]
 console.log("a2:", a2); // [4, 5, 6]
+
+// Define constants
+const hoursPerDay = 2;
+const workingDaysPerWeek = 5;
+const weeksPerYear = 52;
+
+// Calculate total hours
+const totalHours = hoursPerDay * workingDaysPerWeek * weeksPerYear;
+console.log(totalHours);
